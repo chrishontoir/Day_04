74b911c23ec28bdacf9f77d39fa894d42aac0774
Conor <cwilson0005@hotmail.co.uk>
2018-11-29 14:49:35 +0000

    Third test passed, true and false

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -5,3 +5,12 @@ end
 def get_tv(person)
   return person[:favourites][:tv_show]
 end
+
+def get_food(person, food)
+  for snack in person[:favourites][:snacks]
+  if food == snack
+    return true
+  end
+  end
+  return false
+end
